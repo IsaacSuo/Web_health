1d20b8ea3d0579a91202f48e1cf977652f05662a
laoxie72 <744286652@qq.com>
2025-08-02 20:43:17 +0800

    bugs fixed

--- main/urls.py
+++ main/urls.py
@@ -28,7 +28,10 @@ urlpatterns = [
     
     # 认证相关
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    #path('logout/', auth_views.LogoutView.as_view(template_name='/'), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
+    path('accounts/profile/', views.profile, name='profile'),
+
     path('password-change/', auth_views.PasswordChangeView.as_view(template_name='registration/password_change.html'), name='password_change'),
     path('password-change-done/', auth_views.PasswordChangeDoneView.as_view(template_name='registration/password_change_done.html'), name='password_change_done'),
     
